d26e7f6b8c34ca0ec834be1d8b1d0311f6327a08
jirapornmueangsaen <52350347+jirapornmueangsaen@users.noreply.github.com>
2019-08-17 15:15:29 +0700

    Add files via upload

---
+++ Exercise 4_jiraporn_M.py
@@ -0,0 +1,9 @@
+f=85
+g=80
+i=75
+c=65
+print("------Your Score------")
+print("Foundation English",":",f)
+print("General Business",":",g)
+print("Introduction to Computer",":",i)
+print("Computer Programming",":",c)
